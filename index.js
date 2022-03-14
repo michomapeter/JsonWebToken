d8e21cc8a7406e1e4535f0c80085ace697f81379
michomapeter <56400559+michomapeter@users.noreply.github.com>
2022-03-15 01:48:50 +0300

    add files

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.get('/api' , (req,res)=> {
+app.get('/' , (req,res)=> {
     res.json({
         message: 'Welcome to the Api'
     });
 });
+
+app.post('/api/posts' ,verifyToken, (req ,res) => {
+    res.json({
+      message: 'Post created'
+    });
+});
+
+app.post('/api/login' , (req ,res) => {
+
+    const user = {
+        id: 1,
+        username: 'michoma',
+        email:'michomapeter67@gmail.com'
+    }
+    jwt.sign({user: user}, 'secretkey' , (err, token) =>{res.json({token})});
+});
+
+function verifyToken(req ,res ,next){
+    const bearerHeader = req.headers[authorization];
+    if(typeof bearerHeader !== 'undefined'){
+
+    }else{
+        res.sendStatus(403);
+    }
+
+}
 app.listen(5000, () => console.log ('Server started on port 5000')); 
